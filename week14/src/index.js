e2d0d0302cbe071f341f5b3e537809fccf689411
evanjschmitt <evanjschmitt@gmail.com>
2024-12-02 11:11:38 -0500

    WK14 Cart Functions

--- week14/src/index.js
+++ week14/src/index.js
@@ -1,42 +1,25 @@
 import * as $ from "jquery";
-import "./model.js";
+import { initURLListener } from "./model.js"
 
-function changeRoute() {
-let hashTag = window.location.hash;
- let pageID = hashTag.replace('#', '');
-//   console.log(hashTag + ' ' + pageID);
 
-if (pageID != '') {
-$.get(`pages/${pageID}.html`, function (data) {
-//  console.log('data ' + data);
- $('#app').html(data);
-});
-} else {
-$.get(`pages/home.html`, function (data) {
-// console.log('data ' + data);
- $('#app').html(data);
-});
-}
-}
+
+
 
 function initNavlistners() {
-    //Account Listener
-    $(".account").on("click", function() {
-        console.log("Account Clicked");
-    })
-    //Cart Listener
-    $(".cart").on("click", function() {
-        console.log("Cart Clicked (you capitalist)")
-    })
+  //Account Listener
+  $(".account").on("click", function () {
+    window.location.hash = "account";
+    console.log("Account Clicked");
+  });
+  //Cart Listener
+  $(".cart").on("click", function () {
+    window.location.hash = "cart";
+    console.log("Cart Clicked (you capitalist)");
+  });
 }
 
-
-function initURLListener() {
-$(window).on('hashchange', changeRoute);
-changeRoute();
-}
 
 $(document).ready(function () {
-initURLListener();
-initNavlistners();
+  initURLListener();
+  initNavlistners();
 });
--- week14/src/model.js
+++ week14/src/model.js
@@ -1,1 +1,79 @@
 import * as $ from "jquery";
+
+let products = [];
+let cart= [];
+
+function loadHomePage() {
+  $("#app").html("");
+  $.each(products, (index, product) => {
+    let productHTML = `<div class="product">
+    <img src="${product.productImage}" alt="placeholder" />
+    <section class="desc"> ${product.productDesc}
+    </section>
+    <div id=${index} class="buy">Buy Now!</div>
+  </div>`;
+  console.log(productHTML);
+  $("#app").append(productHTML);
+  });
+}
+
+function loadProducts() {
+  $.getJSON("data/data.json", (data) => {
+    products = data.PRODUCTS;
+    console.log(products);
+  });
+}
+
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
+
+  if (pageID != "") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      $("#app").html(data);
+
+        if (pageID == "cart") {
+            if(cart.length > 0) {
+                $.each(cart, (index, product) => {
+                    let cartHtml = `<div class="product">
+                    <img src="${product.productImage}" alt="placeholder" />
+                    <section class="desc"> ${product.productDesc}
+                    </section>
+                    <div id=${index} class="buy">Buy Now!</div>
+                  </div>`;
+                  console.log(cartHtml);
+                  $("#app").append(cartHtml);
+                })
+            }
+        }
+
+
+
+    });
+  }
+    else {
+        if (products.length <= 0) {
+            loadProducts();
+        } else {
+            loadHomePage();
+        }
+    $.get(`pages/home.html`, function (data) {
+      $("#app").html(data);
+    });
+  }
+}
+
+function addBuyNowListener() {
+    $(".buy").on("click", function() {
+        let index = $(this).attr("id");
+        cart.push(index);
+        $(".cartList").html(cart.length);
+        console.log(index);
+    })
+}
+
+export function initURLListener() {
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+}
